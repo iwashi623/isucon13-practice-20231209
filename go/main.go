cc104b80dfca34ad24a25ad6e8279257414ae509
iwashi623 <wonderland.7878@gmail.com>
2023-12-09 15:29:07 +0900

    makefile

--- go/main.go
+++ go/main.go
@@ -97,7 +97,8 @@ func connectDB(logger echo.Logger) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	db.SetMaxOpenConns(10)
+	db.SetMaxOpenConns(64)
+	db.SetMaxIdleConns(64)
 
 	if err := db.Ping(); err != nil {
 		return nil, err
